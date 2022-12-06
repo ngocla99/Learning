78464f1974a41d8c34dd29f159dee1dd8cee78a2
ngoclias <ngoc.leanh1999@gmail.com>
2022-12-06 22:48:53 +0700

    chartjs/area line chart

--- ChartJsDemo/src/App.js
+++ ChartJsDemo/src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
 import {
+  CategoryScale,
   Chart as ChartJS,
+  Filler,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import chartTrendline from 'chartjs-plugin-trendline';
+import React from 'react';
+
+import { Line } from 'react-chartjs-2';
+import BarChar from './components/chart/bar-char/BarChar';
+import { htmlLegendPlugin } from './util/common-function';
+
+ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
@@ -8,95 +24,94 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import chartTrendline from 'chartjs-plugin-trendline';
-
-import { Line } from 'react-chartjs-2';
-import BarChar from './components/chart/bar-char/BarChar';
-import { externalTooltipHandler } from './util/common-function';
-import AntdSelect from './components/antdComps/select';
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, chartTrendline);
+  Filler,
+  chartTrendline,
+  htmlLegendPlugin,
+);
 
 export const options = {
   responsive: true,
   plugins: {
+    htmlLegend: {
+      containerID: 'legend-container',
+    },
     legend: {
-      align: 'end',
-      padding: {
-        bottom: 30,
-      },
-      labels: {
-        usePointStyle: true,
-        pointStyle: 'circle',
-        padding: 30,
-        boxHeight: 8,
-        pointStyleWidth: 10,
-      },
+      // align: 'end',
+      // padding: {
+      //   bottom: 30,
+      // },
+      // labels: {
+      //   usePointStyle: true,
+      //   pointStyle: 'circle',
+      //   padding: 30,
+      //   boxHeight: 8,
+      //   pointStyleWidth: 10,
+      // },
+      display: false,
     },
-    title: {
-      display: true,
-      align: 'start',
-      text: 'Chart.js Line Chart',
-      font: {
-        size: 10,
-      },
-      padding: {
-        bottom: -150,
-        left: 10,
-      },
-    },
-    tooltip: {
-      enabled: false,
-      position: 'nearest',
-      external: externalTooltipHandler,
-    },
+    // title: {
+    //   display: true,
+    //   align: 'start',
+    //   text: 'Chart.js Line Chart',
+    //   font: {
+    //     size: 10,
+    //   },
+    //   padding: {
+    //     bottom: -150,
+    //     left: 10,
+    //   },
+    // },
+    // tooltip: {
+    //   enabled: false,
+    //   position: 'nearest',
+    //   // external: externalTooltipHandler,
+    // },
   },
-  scales: {
-    x: {
-      grid: {
-        drawBorder: false,
-        display: false,
-        drawTicks: false,
-      },
-      ticks: {
-        align: 'start',
-        maxTicksLimit: 4,
-      },
-      title: {
-        display: true,
-        text: 'Time',
-        font: {
-          size: 16,
-          weight: 'bold',
-        },
-      },
-    },
-
-    y: {
-      grid: {
-        borderDash: (context) => {
-          if (context.tick.value === 0) return [];
-          return [4, 4];
-        },
-        color: '#348632',
-        drawTicks: false,
-      },
-      ticks: {
-        crossAlign: 'center',
-        align: 'center',
-        padding: 20,
-      },
-      title: {
-        display: true,
-        text: 'Time',
-        font: {
-          size: 16,
-          weight: 'bold',
-        },
-      },
-    },
-  },
+  plugins: [htmlLegendPlugin],
+  // scales: {
+  //   x: {
+  //     grid: {
+  //       drawBorder: false,
+  //       display: false,
+  //       drawTicks: false,
+  //     },
+  //     ticks: {
+  //       align: 'start',
+  //       maxTicksLimit: 4,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: 'Time',
+  //       font: {
+  //         size: 16,
+  //         weight: 'bold',
+  //       },
+  //     },
+  //   },
+  //   y: {
+  //     grid: {
+  //       borderDash: (context) => {
+  //         if (context.tick.value === 0) return [];
+  //         return [4, 4];
+  //       },
+  //       color: '#348632',
+  //       drawTicks: false,
+  //     },
+  //     ticks: {
+  //       crossAlign: 'center',
+  //       align: 'center',
+  //       padding: 20,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: 'Time',
+  //       font: {
+  //         size: 16,
+  //         weight: 'bold',
+  //       },
+  //     },
+  //   },
+  // },
 };
 
 const labels = [
@@ -123,25 +138,27 @@ export const data = {
       label: 'Dataset 1',
       data: [17, 18, 153, 106, 10, 95, 17, 18, 153, 106, 10, 95, 10, 95],
       borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      trendlineLinear: {
-        colorMin: 'red',
-        lineStyle: 'dotted',
-        width: 1,
-        projection: true,
-      },
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.1)',
+      // trendlineLinear: {
+      //   colorMin: 'red',
+      //   lineStyle: 'dotted',
+      //   width: 1,
+      //   projection: true,
+      // },
     },
     {
       label: 'Dataset 2',
       data: [67, 80, 53, 10, 10, 50],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      trendlineLinear: {
-        colorMax: 'green',
-        lineStyle: 'dotted',
-        width: 2,
-        projection: true,
-      },
+      fill: false,
+      // trendlineLinear: {
+      //   colorMax: 'green',
+      //   lineStyle: 'dotted',
+      //   width: 2,
+      //   projection: true,
+      // },
     },
   ],
 };
@@ -154,10 +171,10 @@ const App = () => {
 
   return (
     <div style={{ width: '1000px', height: '200px' }}>
-      {/* <Line id='ngoclias' options={options} data={data} />
+      <Line id='ngoclias' options={options} data={data} redraw={true} plugins={htmlLegendPlugin} />
       <span>Group of user</span>
-      <BarChar /> */}
-      <AntdSelect />
+      <BarChar />
+      {/* <AntdSelect /> */}
     </div>
   );
 };
