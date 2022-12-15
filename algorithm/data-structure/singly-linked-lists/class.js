b4d444a98a79a8bd635c30eda1151bd7efc3b622
ngoclias <ngoc.leanh1999@gmail.com>
2022-12-15 22:01:56 +0700

    algorithm/add method linked list

--- algorithm/data-structure/singly-linked-lists/class.js
+++ algorithm/data-structure/singly-linked-lists/class.js
@@ -50,9 +50,103 @@ class SinglyLinkedList {
         }
         return current;
     }
+
+    shift() {
+        if (this.length === 0) return undefined;
+
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    unshift(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index > this.length || index < 0) return undefined;
+        let current = this.head;
+        let count = 0;
+        while (count !== index) {
+            current = current.next;
+            count++;
+        }
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (!foundNode) return false;
+        foundNode.val = val;
+        return true;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) {
+            this.push(val);
+            return true;
+        }
+        if (index === 0) {
+            this.push(val);
+            return true;
+        }
+        const newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index > this.length) return undefined;
+        if (index === this.length - 1) return this.pop();
+        if (index === 0) return this.shift();
+
+        let prev = this.get(index - 1);
+        let removed = prev.next;
+        prev.next = removed.next;
+        this.length--;
+
+        return removed;
+    }
+
+    reverse() {
+        let current = this.head;
+        let next;
+        for (let i = 0; i < this.length - 1; i++) {
+            let prev = current;
+            current = prev.next;
+            next = current.next;
+            current.next = prev;
+        }
+    }
 }
 
 let list = new SinglyLinkedList();
-list.push('Hello').push('Hi').push('baby');
-console.log(list.pop());
+list.push('Hello');
+list.push('12');
+list.push('43');
+// console.log(list.pop());
+// console.log(list.shift());
+// list.unshift('Ngoc');
+// console.log(list.get(23));
+// list.set(2, 'KK');
+list.remove(-1);
 console.log(list);
